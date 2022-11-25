2cc68634f2bdc287d67a6927c42442d49911f080
Rishika Gupta <73276425+rishika-afk@users.noreply.github.com>
2022-11-25 20:51:28 +0530

    Create client.py

---
+++ Simple TCP IP Chat App/client.py
@@ -0,0 +1,13 @@
+import socket
+
+s = socket.socket(AF_INET,SOCK_DGRAMsa)
+host = socket.gehostname()
+port = 1616
+
+s.connect((host,port))
+
+print("server says:",s.recv(1024).decode())
+i = input()
+s.send(i.encode())
+print(s.recv(1024).decode())
+s.close()
