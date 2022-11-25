2e5754edae6c0ac0af5fb18489238d8749343623
Rishika Gupta <73276425+rishika-afk@users.noreply.github.com>
2022-11-25 20:51:14 +0530

    Create server.py

---
+++ Simple TCP IP Chat App/server.py
@@ -0,0 +1,20 @@
+#Simple TCP client server application
+import socket
+
+s =  socket.socket()
+host = socket.gethostname()
+port = 1616
+
+s.bind((host,port))
+
+s.listen(5)
+
+while True:
+    c,addr = s.accept()
+    print("Got connection from", addr)
+    c.send("Thank you for connecting, send your message".encode())
+    data = c.recv(1024).decode()
+    print("from connected user: ",str(data))
+    c.send("Message printed on console successfully.".encode())
+    c.close()
+    
